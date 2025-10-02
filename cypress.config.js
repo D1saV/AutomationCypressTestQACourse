1d2135e302f7cef1cc4006eb5a8401146e53c117
D1saV <deonisiyWebDev@gmail.com>
2025-10-02 23:29:25 +0300

    HW Cypress end

--- cypress.config.js
+++ cypress.config.js
@@ -2,6 +2,15 @@ import { defineConfig } from "cypress";
 
 export default defineConfig({
   e2e: {
+    "baseUrl": "https://example.com",
+    "defaultCommandTimeout": 5000,
+    "fixturesFolder": "cypress/fixtures",
+    "specPattern": "**/*.cy.{js,jsx,ts,tsx}",
+    "viewportWidth": 1280,
+    "viewportHeight": 720,
+    "screenshotsFolder": "cypress/screenshots",
+    "videosFolder": "cypress/videos",
+    "modifyObstructiveCode": false,
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
