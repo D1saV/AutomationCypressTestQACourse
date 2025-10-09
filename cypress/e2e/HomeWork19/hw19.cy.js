40df1b5365bde020a82554cac36cba51e00c9ea3
Denis Vasilevskyi <dvasilevskyi@payforce.ua>
2025-10-09 16:16:21 +0300

    add searching footer elements for homework 19

--- cypress/e2e/HomeWork19/hw19.cy.js
+++ cypress/e2e/HomeWork19/hw19.cy.js
@@ -14,24 +14,35 @@ describe('HomeWork19', () => {
 
   it('Find the contacts section elements', () => {
    // social-media find by icon
-cy.get('.icon-facebook').parent()
-cy.get('.icon-telegram').parent()
-cy.get('.icon-youtube').parent()
-cy.get('.icon-instagram').parent()
-cy.get('.icon-linkedin').parent()
-    //social-media find by link
-cy.get('a[href*="facebook.com"]')
-cy.get('a[href*="t.me"]')
-cy.get('a[href*="youtube.com"]')
-cy.get('a[href*="instagram.com"]')
-cy.get('a[href*="linkedin.com"]')
+    cy.get('.icon-facebook').parent()
+    cy.get('.icon-telegram').parent()
+    cy.get('.icon-youtube').parent()
+    cy.get('.icon-instagram').parent()
+    cy.get('.icon-linkedin').parent()
+        //social-media find by link
+    cy.get('a[href*="facebook.com"]')
+    cy.get('a[href*="t.me"]')
+    cy.get('a[href*="youtube.com"]')
+    cy.get('a[href*="instagram.com"]')
+    cy.get('a[href*="linkedin.com"]')
 
     //hillel links on web by class
-cy.get('[class="contacts_link display-4"]').contains('ithillel.ua')
-cy.get('[class="contacts_link h4"]').contains('support@ithillel.ua')
-    //hillel links on web by link
-cy.get('a[href*="ithillel.ua"]').contains('ithillel.ua')
-cy.get('a[href*="mailto:developer@ithillel.ua"]').contains('support@ithillel.ua')
+    cy.get('[class="contacts_link display-4"]').contains('ithillel.ua')
+    cy.get('[class="contacts_link h4"]').contains('support@ithillel.ua')
+        //hillel links on web by link
+    cy.get('a[href*="ithillel.ua"]').contains('ithillel.ua')
+    cy.get('a[href*="mailto:developer@ithillel.ua"]').contains('support@ithillel.ua')
+  })
+
+  it('Find the footer elements', () => {
+    //for left elements
+    cy.get('.footer_item.-left').within(() => {
+    cy.get('p').eq(0).should('contain', '© 2021 Hillel IT school')
+    cy.get('p').eq(1).should('contain', 'Hillel auto developed')
+    })
+    //for right elements
+    cy.get('.footer_item.-right').find('.footer_logo')
+    
   })
 })
 
